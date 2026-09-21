c4faf483748fd69183238ad11b355430048bb998
Kevin Andres Mancera Orjuela <93.kevinmancera@gmail.com>
2026-09-20 21:34:20 -0500

    feat: update tmux conf
    - TMUX and nvim move CTRL
    - update kanagawa theme on kitty, tmux
    - tmux plugins update

--- .zshrc
+++ .zshrc
@@ -94,11 +94,11 @@ spaceship add --before char vi_mode
 # export LANG=en_US.UTF-8
 
 # Preferred editor for local and remote sessions
-# if [[ -n $SSH_CONNECTION ]]; then
-#   export EDITOR='vim'
-# else
-#   export EDITOR='mvim'
-# fi
+if [[ -n $SSH_CONNECTION ]]; then
+  export EDITOR='vim'
+else
+  export EDITOR='nvim'
+fi
 
 # Compilation flags
 # export ARCHFLAGS="-arch x86_64"
--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -13,4 +13,22 @@ return {
   },
   { "dstein64/vim-startuptime", cmd = "StartupTime" },
   { "mbbill/undotree" },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  }
 }
